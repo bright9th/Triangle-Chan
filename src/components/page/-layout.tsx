f058f6c517159ff2854f19ee2fc9f56473afc21c
BrightNighter <129858054+bright9th@users.noreply.github.com>
2026-07-21 19:53:27 +0700

    Triangle

--- src/components/page/-layout.tsx
+++ src/components/page/-layout.tsx
@@ -1,29 +1,15 @@
-import { ThemeSwitcher } from "../common/theme";
-import ViteIcon from "/assets/vite.svg";
+// import { ThemeSwitcher } from "../common/theme";
+// import ViteIcon from "/assets/vite.svg";
 
 const Layout = () => {
   return (
-    <div
-      className="flex flex-col w-full h-full
-      bg-gradient-to-b from-[var(--bg-a2)] to-white
-      [.dark_&]:from-[var(--bg-a2-dark)] [.dark_&]:to-black"
-    >
-      <div className="relative w-full h-screen flex justify-center items-center">
-        <div
-          className="absolute animate-spin p-32
-          rounded-full border-8 border-t-transparent"
-        ></div>
-        <div
-          className="absolute animate-spin p-24
-          rounded-full border-6 border-b-transparent"
-        ></div>
-
-        <img src={ViteIcon} className="absolute max-w-[50vw] max-h-[50vh]" />
-      </div>
-
-      <div className="absolute top-5 end-5">
-        <ThemeSwitcher />
-      </div>
+    <div className="w-screen h-screen bg-white flex items-center justify-center">
+      <div
+        className="animate-spin
+          w-0 h-0
+          border-l-[96px] border-r-[96px] border-b-[168px]
+          border-l-transparent border-r-transparent border-b-[#FF0000]"
+      />
     </div>
   );
 };
